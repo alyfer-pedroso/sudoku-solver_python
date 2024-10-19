0b8218e011ccad97b989230a1d9eb7ffde35067c
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2024-10-19 00:48:43 -0300

    Remoção do print dinâmico e da exclusão automática dos valores nas células.

--- sudoku.py
+++ sudoku.py
@@ -93,8 +93,8 @@ def generate_sudoku(self, size):
     squares = size * size
     empties = squares * 3 // 4
 
-    for p in random.sample(range(squares), empties):
-        board[p // size][p % size] = 0
+    # for p in random.sample(range(squares), empties):
+    #     board[p // size][p % size] = 0
 
     gc.collect()
 
